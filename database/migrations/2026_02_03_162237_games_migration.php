9d5209af7aebcbf945a126e7e56754ec22b799d5
Amrudin Balić <101043743+amrudinbalich@users.noreply.github.com>
2026-02-03 20:16:10 +0100

    Games Entity Migtation (#3)
    Co-authored-by: Amrudin Balić <amrudinbalic@192.168.1.7>

---
+++ database/migrations/2026_02_03_162237_games_migration.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('games', function (Blueprint $table) {
+            $table->id();
+            $table->string('title')->unique();
+            $table->string('description')->nullable();
+            $table->string('player_objective')->nullable();
+            $table->boolean('mature_content_warning')->default(false);
+            $table->foreignId('publisher_id')->constrained('publishers')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('games');
+    }
+};
